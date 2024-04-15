d0c37612b4d3e72b42dc12ad44f2d28b90ac7d1d
P S Shrijith <shrijithps89@gmail.com>
2024-04-15 07:38:40 +0530

    navigation

--- cypress/Cypress/e2e/navigation.cy.js
+++ cypress/Cypress/e2e/navigation.cy.js
@@ -1,19 +1,21 @@
 describe("Navigation Suite", ()=>{
     it("navigation", () =>{
         cy.visit("demo.opencart.com");
+
         cy.contains('a', "Tablets").click();
         cy.get("#compare-total").should('be.visible');
         cy.wait(2000);
+
         cy.go('back');
         cy.wait(2000);
         cy.contains('h3', 'Featured').should('be.visible');
+
         cy.go(+1);
         cy.wait(1000);
         cy.get("#compare-total").should('be.visible');
+
         cy.wait(1000);
         cy.go(-1);
         cy.contains('h3', 'Featured').should('be.visible');
-
-
     })
 })
