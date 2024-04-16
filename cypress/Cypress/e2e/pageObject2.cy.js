cd5fcfab8488ca5b2ff763221400a0c94c29cbab
P S Shrijith <shrijithps89@gmail.com>
2024-04-16 22:47:18 +0530

    newchanges

---
+++ cypress/Cypress/e2e/pageObject2.cy.js
@@ -0,0 +1,12 @@
+import Login from "../pageObjects/loginPage2.js"
+
+describe("Test Suite", ()=> {
+    it("Page Object", ()=>{
+        cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index")
+        const test = new Login();
+        test.setUserName("Admin")
+        test.setPassword("admin123")
+        test.clickLogin();
+        test.verifyLogin();
+    })
+})
